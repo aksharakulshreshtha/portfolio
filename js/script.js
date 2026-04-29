eb49d3c8b233f0b5fa189bd550fb990448ed8116
aksharakulshreshtha <aksharakulshreshtha@gmail.com>
2026-04-29 21:17:39 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+// NAV ACTIVE STATE
+const navItems = document.querySelectorAll("nav ul li");
 
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navItems.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    });
+});
+
+
+// SMOOTH SCROLL (optional future use)
+document.querySelectorAll("nav ul li").forEach(item => {
+    item.addEventListener("click", () => {
+        console.log(item.innerText + " clicked");
+    });
+});
+
+
+// BUTTON CLICK
+document.querySelector("button").addEventListener("click", () => {
+    alert("Download started (dummy)");
+});
